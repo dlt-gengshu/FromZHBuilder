97ca207657f3c6fa15cf5c17eaca3c93519138de
dlt-gengshu <dlt_gengshu@163.com>
2016-08-01 15:50:08 +0800

    The First.

--- FromZHBuilder/Form1.Designer.cs
+++ FromZHBuilder/Form1.Designer.cs
@@ -28,10 +28,27 @@ namespace FromZHBuilder
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new Gizmox.WebGUI.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(31, 27);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            // 
+            // Form1
+            // 
+            this.Controls.Add(this.button1);
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private Gizmox.WebGUI.Forms.Button button1;
     }
 }
